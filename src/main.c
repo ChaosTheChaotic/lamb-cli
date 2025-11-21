f88ffdcb90358124cbda28497f5b4cfec6d9209b
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-21 18:00:46 +0000

    Add simple argument parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
+#include <strings.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-  printf("Hello, world!");
+/**
+ * A function which parses the arguments for the type of subcommand the binary is running
+ *
+ * @param argc the argument count
+ * @param argv an array of all the arguments
+ * @return false on an invalid arg else true
+*/
+bool parseArgs(int argc, char **argv) {
+  for (unsigned i = 0; i < argc; i++) {
+  }
+  return true;
+}
+
+/**
+  * A function which parses the type of subcommand the binary should run
+  *
+  * @param argc the amount of args (the argument count)
+  * @param argv an array of all the arguments
+  * @return a boolean which is true if we found a valid subcommand else false
+*/
+bool parseType(int argc, char **argv) {
+  for (unsigned i = 0; i < argc; i++) {
+    if (strcasecmp(argv[i], "send")) return true;
+    if (strcasecmp(argv[i], "request")) return true;
+  }
+  return false;
+}
+
+int main(int argc, char **argv) {
+  bool succ = parseType(argc, argv);
+  if (!succ) {
+    printf("Failed to get a valid subcommand of lambc");
+  } else {
+    printf("Completed operation");
+  }
   return 0;
 }
