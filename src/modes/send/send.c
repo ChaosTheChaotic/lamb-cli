a4987e9cf8189faa4fc7e26eb133068c6bc66e11
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-23 15:09:09 +0000

    Start working on send mode

--- src/modes/send/send.c
+++ src/modes/send/send.c
@@ -1,5 +1,20 @@
 #include "libcrocou.h"
 #include "subcmd.h"
 #include "pwds.h"
+#include <string.h>
 
-int send(int argc, Arg *args) {}
+int send(unsigned int argc, Arg *args) {
+  char *msg = NULL;
+  char *pwd = NULL;
+
+  for (unsigned int i = 0; i < argc; i++) {
+    if (args[i].flag == 'm' && args[i].found) {
+      msg = args[i].value.str_val;
+      break;
+    }
+  }
+
+  if (msg == NULL || strcmp(msg, "Hello, world!") == 0) {
+    // Prompt user for message input here
+  }
+}
