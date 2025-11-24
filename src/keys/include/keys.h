d47ee6a88b00d2c7e0f5251ec5c43fc7db8e59bf
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-24 20:49:28 +0000

    Add keyctl for storing passwords temporarily

---
+++ src/keys/include/keys.h
@@ -0,0 +1,10 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+int store_pwd(char *restrict pwd);
+
+char *get_pwd();
+
+int clear_pwd();
+
+#endif
---
+++ src/keys/keys.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <linux/keyctl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include "keys.h"
+
+int store_pwd(char *restrict pwd) {
+  if (!pwd) return -1;
+
+  int kid = syscall(SYS_add_key, "user", "lamb_session_password", pwd, strlen(pwd), KEY_SPEC_USER_SESSION_KEYRING);
+  return kid;
+}
+
+char *get_pwd() {
+  int kid = syscall(SYS_keyctl, KEYCTL_SEARCH, KEY_SPEC_USER_SESSION_KEYRING, "user", "lamb_session_password", 0);
+  if (kid < 0) {
+    return NULL;
+  }
+
+  long klen = syscall(SYS_keyctl, KEYCTL_READ, kid, NULL, 0);
+  if (klen < 0) {
+    return NULL;
+  }
+
+  char *buf = (char *)malloc(klen + 1);
+  if (!buf) {
+    return NULL;
+  }
+
+  long ret = syscall(SYS_keyctl, KEYCTL_READ, kid, buf, klen);
+  if (ret < 0) {
+    free(buf);
+    return NULL;
+  }
+
+  buf[klen] = '\0';
+  return buf;
+}
+
+// Just in case we need it
+int clear_pwd(void) {
+    int key_id = syscall(SYS_keyctl, KEYCTL_SEARCH, KEY_SPEC_SESSION_KEYRING, "user", "session_password", 0);
+    if (key_id < 0) {
+        return -1;
+    }
+    
+    return syscall(SYS_keyctl, KEYCTL_UNLINK, key_id, KEY_SPEC_SESSION_KEYRING);
+}
