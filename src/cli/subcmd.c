a53eaf4dacfacfaaab2cfb2173bc3b8e6e17bb2c
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-24 22:00:57 +0000

    Remove useless CLI flag

--- src/cli/subcmd.c
+++ src/cli/subcmd.c
@@ -1,14 +1,13 @@
 #include "subcmd.h"
 
 Arg send_args[] = {
-    {'a', BOOL, false, {.bool_val = false}, false}, // TODO: Implement async
     {'m', STRING, false, {.str_val = ""}, false}, // Message flag
     {'p', BOOL, false, {.bool_val = false}, false}, // Password flag (specify a new password to save)
 };
 const int send_arg_count = sizeof(send_args) / sizeof(Arg);
 
 Arg request_args[] = {
-    {'a', BOOL, false, {.bool_val = false}, false},
+    {'p', BOOL, false, {.bool_val = false}, false},
 };
 const int request_arg_count = sizeof(request_args) / sizeof(Arg);
 
